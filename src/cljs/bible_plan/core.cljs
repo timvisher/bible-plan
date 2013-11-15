e4514670d7de82acb4cb34dab379c75ef19c77e7
Tim Visher <tvisher@monetate.com>
2013-11-15 00:32:42 -0500

    Danny helped me listen to the form only. EOM

--- src/cljs/bible_plan/core.cljs
+++ src/cljs/bible_plan/core.cljs
@@ -10,16 +10,7 @@
   (in-ns 'bible-plan.core)
   )
 
-(dom/listen! (sel1 (keyword "input[name=plan]")) :change plan-ui/re-show-plan)
-
-(doseq [skip-day-node (sel (keyword "input[name=skip-day]"))]
-  (dom/listen! skip-day-node :change plan-ui/re-show-plan))
-
-(doseq [books-at-a-time-node (sel (keyword "input[name=books-at-a-time]"))]
-  (dom/listen! books-at-a-time-node :change plan-ui/re-show-plan))
-
-(doseq [books-at-a-time-node (sel (keyword "input[name=books-at-a-time]"))]
-  (dom/listen! books-at-a-time-node :change plan-ui/re-show-plan))
+(dom/listen! (sel1 :form) :change plan-ui/re-show-plan)
 
 (comment
   (dom/event-listeners (sel1 (keyword "input[name=plan]")))
