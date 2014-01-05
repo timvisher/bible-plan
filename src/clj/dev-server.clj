c417d460c534c19b9cb99e51b349e6081e070d2e
Tim Visher <tvisher@monetate.com>
2014-01-05 18:33:25 -0500

    Make dev-server more pleasent to use from REPL. EOM

--- src/clj/dev-server.clj
+++ src/clj/dev-server.clj
@@ -37,14 +37,17 @@
 
 (run)
 
-(comment
+(defn create-repl-env []
   (def repl-env (reset! cemerick.austin.repls/browser-repl-env
-                        (cemerick.austin/repl-env)))
+                        (cemerick.austin/repl-env))))
 
-  ;; (def repl-env (reset! cemerick.austin.repls/browser-repl-env nil))
+(defn kill-repl-env []
+  (def repl-env (reset! cemerick.austin.repls/browser-repl-env nil)))
 
-  ;; Only run after you've loaded the page
-  (cemerick.austin.repls/cljs-repl repl-env)
+(defn attach-to-repl []
+  (cemerick.austin.repls/cljs-repl repl-env))
+
+(comment
 
   (js/alert "Charnock!")
   )
