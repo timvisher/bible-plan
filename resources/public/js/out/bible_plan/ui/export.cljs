80d719bf3955f4069885fef764597a25647092fe
Tim Visher <tvisher@monetate.com>
2014-02-10 22:01:42 -0500

    Wire submit of export form to export function. EOM

---
+++ resources/public/js/out/bible_plan/ui/export.cljs
@@ -0,0 +1,5 @@
+(ns bible-plan.ui.export
+  (:require [shodan.console :as console]))
+
+(defn show-plan-exporter []
+  (console/log "Ohai, charnock!"))
---
+++ resources/public/whitespace/js/out/bible_plan/ui/export.js
@@ -0,0 +1,7 @@
+// Compiled by ClojureScript 0.0-2156
+goog.provide('bible_plan.ui.export$');
+goog.require('cljs.core');
+goog.require('shodan.console');
+goog.require('shodan.console');
+bible_plan.ui.export$.show_plan_exporter = (function show_plan_exporter(){return shodan.console.log.call(null,"Ohai, charnock!");
+});
